3907f27015a24144f1ff1697620c36b4ce771b25
Damien Rubio <rubio.damien@gmail.com>
2019-10-30 16:18:02 +0100

    remove base url

--- src/main/java/me/skrib/users/web/UserResource.java
+++ src/main/java/me/skrib/users/web/UserResource.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping
 public class UserResource {
 
     private final UserService userService;
